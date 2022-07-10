04c81886a0029c58e907b97f156eada2dd3edd03
gabrieletorres <secgabriele@gmail.com>
2022-07-10 20:51:20 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+
+    identification:
+    {
+      nome: 'Gabriele',
+      idade: 22,
+      comidaFavorita: 'Risotto',
+      músicasFavoritas: ['Lovesick', 'Peripeteia', 'Matchbox']
+    }
+  }
+
+  render(){
+    return(
+      <div>
+      <h1>Nome: {this.state.identification.nome}</h1>
+      <h2>Idade: {this.state.identification.idade}</h2>
+      <h3>Comida favorita: {this.state.identification.comidaFavorita}</h3>
+      
+      <p>Músicas favoritas:</p>
+      <ul>
+        <li>{this.state.identification.músicasFavoritas[0]}</li>
+        <li>{this.state.identification.músicasFavoritas[1]}</li>
+        <li>{this.state.identification.músicasFavoritas[2]}</li>
+      </ul> 
+      </div>
+      )
+  }
 }
 
 export default App;
